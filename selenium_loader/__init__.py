677d6ea6b59e8e7083ea859175f4071c382b1677
Kevin Guzman <kevocde@gmail.com>
2023-02-24 08:12:53 -0500

    [updated] version to 0.1.6

--- selenium_loader/__init__.py
+++ selenium_loader/__init__.py
@@ -1,6 +1,6 @@
 """Configuration loader and action executor for Selenium"""
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from .action_executor import ActionExecutor
 from .config import BrowserType, Config
