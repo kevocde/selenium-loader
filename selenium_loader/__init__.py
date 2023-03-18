c7a3b0d566251585b9c23bfa97261cdca3531fdb
kevocde <kevocde@gmail.com>
2023-03-18 10:30:34 -0500

    [updated] version

--- selenium_loader/__init__.py
+++ selenium_loader/__init__.py
@@ -1,6 +1,6 @@
 """Configuration loader and action executor for Selenium"""
 
-__version__ = "0.2.0"
+__version__ = "0.3"
 
 from .action_executor import ActionExecutor
 from .utils import BrowserType, Config
